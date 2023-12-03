084441b689422a3b99ca830c03e6691319a8acc7
Vinayaks439 <vinayaks439@gmail.com>
2023-12-03 17:00:12 +0530

    Added testfile for accounts and fulltest

---
+++ db/sqlc/accounts.sql_test.go
@@ -0,0 +1,7 @@
+package db
+
+import "testing"
+
+func TestCreateAccount(T *testing.T) {
+	
+}
---
+++ db/sqlc/full_test.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+	"os"
+	"testing"
+)
+
+const (
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	testQueries = New(conn)
+	os.Exit(m.Run())
+}
