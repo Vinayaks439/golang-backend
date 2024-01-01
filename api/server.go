ac2145bd6996e24797b00b7882e91c16dcbffc76
Vinayaks439 <vinayaks439@gmail.com>
2024-01-01 17:10:43 +0530

    Basic Gin API commit

---
+++ api/server.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	db "github.com/Vinayaks439/golang-backend/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+	router.POST("/v1/accounts", server.createAccount)
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/Vinayaks439/golang-backend/api"
+	db2 "github.com/Vinayaks439/golang-backend/db/sqlc"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+)
+
+const (
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	conn, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	store := db2.NewStore(conn)
+	err = api.NewServer(store).Start("localhost:8080")
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
